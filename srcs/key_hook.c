5ddaee56641d68de95290b92368e4a9d901312b6
Alexandra BUI <alebui@e2r1p3.42.fr>
2020-02-11 16:04:24 +0100

    split key_hook() function in 2 other subfonctions wasd_key() and rotation_key()

--- srcs/key_hook.c
+++ srcs/key_hook.c
@@ -12,65 +12,57 @@
 
 #include <wolf3d.h>
 
-int		check_if_wall(t_env *e, int i)
+// check if wall doesn't work 
+int			check_if_wall(t_env *e, int i)
 {
-	if (e && i)
-		printf("je check\n");
-	printf("je check\n");
-	/*
-	int x;
-	int y;
+	t_pos 	cur; //cur pour cursor
 
-	x = e->player.x * e->bloc_width;
-	y = e->player.y * e->bloc_height;
-//	if (e->dir->x == 1)
-//		y -= 2;
+	cur.x = e->player.x * e->bloc_width;
+	cur.y = e->player.y * e->bloc_height;
+	// if (e->dir->start.x == 1)
+	// 	cur.y -= 2;
 	if (i == 2)
-		y += 2;
+		cur.y += 2;
 	else if (i == 3)
-		x -= 2;
+		cur.x -= 2;
 	else if (i == 4)
-		x += 2;
-	e->current_bloc.x = x / e->bloc_width;
-	e->current_bloc.y = y / e->bloc_height;
+		cur.x += 2;
+	e->current_bloc.x = cur.x / e->bloc_width;
+	e->current_bloc.y = cur.y / e->bloc_height;
 	if (e->current_bloc.y <= e->rows && e->current_bloc.x <= e->col)
 	{
 		if (e->file[e->current_bloc.y][e->current_bloc.x] != 0)
 			return (1);
 	}
-	*/
 	return (0);
 }
 
-int		key_hook(int key, t_env *e)
+static void	wasd_key(int key, t_env *e)
 {
-	if (e)
-		printf("key_hook\n");
-	if (key == MAIN_PAD_ESC)
-	{
-		// mlx_destroy_image(e->mlx, e->ptr); 
-		exit(EXIT_SUCCESS);
-	}
 	if (key == W && e->player.y < HEIGHT && check_if_wall(e, 1) == 0)
 	{
 		e->player.y += e->dir_p.y / 10;
 		e->player.x += e->dir_p.x / 10;
 	}
-	if (key == S && e->player.y > 0 && check_if_wall(e, 2) == 0)
+	else if (key == S && e->player.y > 0 && check_if_wall(e, 2) == 0)
 	{
 		e->player.y -= e->dir_p.y / 10;
 		e->player.x -= e->dir_p.x / 10;
 	}
-	if (key == A && e->player.x > 0 && check_if_wall(e, 3) == 0)
+	else if (key == A && e->player.x > 0 && check_if_wall(e, 3) == 0)
 	{
 		e->player.y -= e->dir_p.x / 10;
 		e->player.x += e->dir_p.y / 10;
 	}
-	if (key == D && e->player.x < WIDTH && check_if_wall(e, 4) == 0)
+	else if (key == D && e->player.x < WIDTH && check_if_wall(e, 4) == 0)
 	{
 		e->player.y += e->dir_p.x / 10;
 		e->player.x -= e->dir_p.y / 10;
 	}
+}
+
+static void	rotation_key(int key, t_env *e)
+{
 	if (key == ARROW_LEFT)
 	{
 		e->oldx = e->dir_p.x;
@@ -80,7 +72,7 @@ int		key_hook(int key, t_env *e)
 		e->plane_p.x = e->plane_p.x * cos(-0.05) - e->plane_p.y * sin(-0.05);
 		e->plane_p.y = e->oldx * sin(-0.05) + e->plane_p.y * cos(-0.05);
 	}
-	if (key == ARROW_RIGHT)
+	else if (key == ARROW_RIGHT)
 	{
 		e->oldx = e->dir_p.x;
 		e->dir_p.x = e->dir_p.x * cos(0.05) - e->dir_p.y * sin(0.05);
@@ -89,6 +81,17 @@ int		key_hook(int key, t_env *e)
 		e->plane_p.x = e->plane_p.x * cos(0.05) - e->plane_p.y * sin(0.05);
 		e->plane_p.y = e->oldx * sin(0.05) + e->plane_p.y * cos(0.05);
 	}
+}
+
+int			key_hook(int key, t_env *e)
+{
+	if (key == MAIN_PAD_ESC)
+	{
+		// mlx_destroy_image(e->mlx, e->ptr); 
+		exit(EXIT_SUCCESS);
+	}
+	wasd_key(key, e);
+	rotation_key(key, e);
 	// mlx_destroy_image(e->mlx, e->ptr);
 	draw_2D_map(e);
 	return (0);
