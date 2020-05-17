bf91fbd1d79d4dbf3b3b233edb7159f40d51143b
Alex Bui <alxbui14@gmail.com>
2020-05-17 19:24:52 +0200

    delete mlx mouse hook function

--- srcs/main.c
+++ srcs/main.c
@@ -24,40 +24,20 @@ int			ft_close(void)
 	return (0);
 }
 
-int			text_init(t_env *e)
-{
-	int a;
-	int b;
-
-	a = 64;
-	b = 64;
-	e->text[0].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/stone1.xpm", &a, &b);
-	e->text[0].data = mlx_get_data_addr(e->text[0].img, &e->text[0].bpp, &e->text[0].sizeline, &e->text[0].endian);
-	e->text[1].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/stone3.xpm", &a, &b);
-	e->text[1].data = mlx_get_data_addr(e->text[1].img, &e->text[1].bpp, &e->text[1].sizeline, &e->text[1].endian);
-	e->text[2].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/stone2.xpm", &a, &b);
-	e->text[2].data = mlx_get_data_addr(e->text[2].img, &e->text[2].bpp, &e->text[2].sizeline, &e->text[2].endian);
-	e->text[3].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/stone4.xpm", &a, &b);
-	e->text[3].data = mlx_get_data_addr(e->text[3].img, &e->text[3].bpp, &e->text[3].sizeline, &e->text[3].endian);
-	e->text[4].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/wood1.xpm", &a, &b);
-	e->text[4].data = mlx_get_data_addr(e->text[4].img, &e->text[4].bpp, &e->text[4].sizeline, &e->text[4].endian);
-	e->text[5].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/wood3.xpm", &a, &b);
-	e->text[5].data = mlx_get_data_addr(e->text[5].img, &e->text[5].bpp, &e->text[5].sizeline, &e->text[5].endian);
-	e->text[6].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/wood2.xpm", &a, &b);
-	e->text[6].data = mlx_get_data_addr(e->text[6].img, &e->text[6].bpp, &e->text[6].sizeline, &e->text[6].endian);
-	e->text[7].img = mlx_xpm_file_to_image(e->mlx->ptr, "textures/wood4.xpm", &a, &b);
-	e->text[7].data = mlx_get_data_addr(e->text[7].img, &e->text[7].bpp, &e->text[7].sizeline, &e->text[7].endian);
-	return (0);
-}
-
 static void	hook_events(t_env *e)
 {
 	mlx_hook(e->mlx->wind, 2, (1L << 0), &key_hook, e);
 	mlx_hook(e->mlx->wind, 3, (1L << 1), &key_release_hook, e);
-	mlx_mouse_hook(e->mlx->wind, mouse_hook, e);
 	mlx_hook(e->mlx->wind, 17, 0L, ft_close, e);
 	mlx_loop(e->mlx->ptr);
 }	
+
+/*
+static void __attribute__((destructor)) destruct()
+{
+	while(1);
+}
+*/
 
 int			main(int ac, char **av)
 {
