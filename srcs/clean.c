9b67ad52de9225229c5acc4af0f18315642e0239
Alex Bui <alxbui14@gmail.com>
2020-05-17 19:44:37 +0200

    function pour clean map_lines au cas ou

--- srcs/clean.c
+++ srcs/clean.c
@@ -12,6 +12,20 @@
 
 #include <wolf3d.h>
 
+static void	clean_map_lines(t_env *e)
+{
+	// printf("e->map_lines = %p\n", e->map_lines);
+	if (e->map_lines)
+	{
+		while (e->map_lines)
+		{
+			free(e->map_lines->nums);
+			e->map_lines->nums = NULL;
+			e->map_lines = e->map_lines->next;
+		}
+	}
+}
+
 static void	clean_text(t_env *e)
 {
 	int		i;
@@ -21,17 +35,17 @@ static void	clean_text(t_env *e)
 	{
 		if (e->text[i].img)
 		{
-			printf("Before: e->text[%d].img = %p\n", i, e->text[i].img);
+			// printf("Before: e->text[%d].img = %p\n", i, e->text[i].img);
 			free(e->text[i].img);
 			e->text[i].img = NULL;
-			printf("After: e->text[%d].img = %p\n", i, e->text[i].img);
+			// printf("After: e->text[%d].img = %p\n", i, e->text[i].img);
 		}
 		if (e->text[i].data)
 		{
-			printf("Before: e->text[%d].img = %p\n", i, e->text[i].data);
+			// printf("Before: e->text[%d].img = %p\n", i, e->text[i].data);
 			free(e->text[i].data);
 			e->text[i].data = NULL;
-			printf("After: e->text[%d].img = %p\n", i, e->text[i].data);
+			// printf("After: e->text[%d].img = %p\n", i, e->text[i].data);
 		}
 		i++;
 	}
@@ -42,37 +56,38 @@ static void	clean_vector(t_env *e)
 	printf("attention je clean vector\n\n");
 	if (e->dir)
 	{
-		printf("Before: e->dir = %p\n", e->dir);
+		// printf("Before: e->dir = %p\n", e->dir);
 		free(e->dir);
 		e->dir = NULL;
-		printf("After: e->dir = %p\n\n", e->dir);
+		// printf("After: e->dir = %p\n\n", e->dir);
 	}
 	if (e->plane)
 	{
-		printf("Before: e->plane = %p\n", e->plane);
+		// printf("Before: e->plane = %p\n", e->plane);
 		free(e->plane);
 		e->plane = NULL;
-		printf("After: e->plane = %p\n\n", e->plane);
+		// printf("After: e->plane = %p\n\n", e->plane);
 	}
 	if (e->lray)
 	{
-		printf("Before: e->lray = %p\n", e->lray);
+		// printf("Before: e->lray = %p\n", e->lray);
 		free(e->lray);
 		e->lray = NULL;
-		printf("After: e->lray = %p\n\n", e->lray);
+		// printf("After: e->lray = %p\n\n", e->lray);
 	}
 	if (e->rray)
 	{
-		printf("Before: e->rray = %p\n", e->rray);
+		// printf("Before: e->rray = %p\n", e->rray);
 		free(e->rray);
 		e->rray = NULL;
-		printf("After: e->rray = %p\n\n", e->rray);
+		// printf("After: e->rray = %p\n\n", e->rray);
 	}
-	printf("j'ai clean\n");
+	// printf("j'ai clean\n");
 }
 
 void		exit_program(t_env *e)
 {
+	clean_map_lines(e);
 	clean_vector(e);
 	clean_text(e);
 	exit(EXIT_SUCCESS);
