6713a41024787e3c070c6d1ff67f74e5bba270d6
Alex Bui <alxbui14@gmail.com>
2020-05-18 15:53:19 +0200

    key robin et alex

--- includes/controls.h
+++ includes/controls.h
@@ -14,6 +14,8 @@
 
 # define CONTROLS_H
 
+/*
+
 # define A 0
 # define B 11
 # define C 8
@@ -78,5 +80,72 @@
 # define MAIN_PAD_MORE 47
 
 #define SPACE_BAR 49
+
+*/
+
+# define A 97
+# define B 11
+# define C 8
+# define D 100
+# define E 4
+# define F 3
+# define G 9
+# define H 4
+# define I 34
+# define J 38
+# define K 40
+# define L 37
+# define M 109
+# define N 45
+# define O 31
+# define P 35
+# define Q 12
+# define R 15
+# define S 115
+# define T 17
+# define U 32
+# define V 9
+# define W 119
+# define X 7
+# define Y 16
+# define Z 6
+
+# define ARROW_UP 65362
+# define ARROW_DOWN 63564
+# define ARROW_LEFT 65361
+# define ARROW_RIGHT 65363
+
+# define NUM_PAD_0 82
+# define NUM_PAD_1 83
+# define NUM_PAD_2 84
+# define NUM_PAD_3 85
+# define NUM_PAD_4 86
+# define NUM_PAD_5 87
+# define NUM_PAD_6 88
+# define NUM_PAD_7 89
+# define NUM_PAD_8 91
+# define NUM_PAD_9 92
+# define NUM_PAD_PLUS 69
+# define NUM_PAD_MINUS 78
+
+# define MAIN_PAD_ESC 65307
+# define MAIN_PAD_I 34
+# define MAIN_PAD_P 35
+# define MAIN_PAD_0 29
+# define MAIN_PAD_1 18
+# define MAIN_PAD_2 19
+# define MAIN_PAD_3 20
+# define MAIN_PAD_4 21
+# define MAIN_PAD_5 23
+# define MAIN_PAD_6 22
+# define MAIN_PAD_7 26
+# define MAIN_PAD_8 28
+# define MAIN_PAD_9 25
+# define MAIN_PAD_PLUS 24
+# define MAIN_PAD_MINUS 27
+# define MAIN_PAD_LESS 43
+# define MAIN_PAD_MORE 47
+
+#define SPACE_BAR 32
 
 #endif
