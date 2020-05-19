27cd742d2e8e8bb0e4bc4ec52d2d08ff2cf4d485
Alex Bui <alxbui14@gmail.com>
2020-05-19 19:24:50 +0200

    modified usage function

--- srcs/error.c
+++ srcs/error.c
@@ -12,16 +12,25 @@
 
 #include <wolf3d.h>
 
-void			usage(char *program)
+void			usage(void)
 {
-	ft_putstr(program);
-	ft_putendl(": usage: filename");
+	ft_putendl("Usage : wolf3d [map01/map02/map03]");
 }
+
+
 
 void			ft_error(void)
 {
 	ft_putstr("error: ");
 	ft_putendl(strerror(errno));
+}
+
+void			v_error_msg(char *message, t_env *env)
+{
+	if (message && VERBOSE)
+		ft_putendl_fd(message, 1);
+	if (env->map_lines)
+		del_map_lines(&env->map_lines);
 }
 
 int				error_msg(char *message, t_env *env)
